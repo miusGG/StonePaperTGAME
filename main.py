b2f3f9179f55a8e623727f4bfc53f403d362d102
miusGG <romanovegorsergeevich@gmail.com>
2024-11-29 17:19:56 +0300

    +inline buttons +jpg send +start game(beta) 0.2

--- main.py
+++ main.py
@@ -13,38 +13,23 @@ from aiogram.types import ReplyKeyboardRemove, \
     ReplyKeyboardMarkup, KeyboardButton, \
     InlineKeyboardMarkup, InlineKeyboardButton
 
-from aiogram.types import CallbackQuery
+from aiogram.types import CallbackQuery, InputMediaPhoto, FSInputFile
 
-from aiogram import Router
-from aiogram import F
+from aiogram import Router, F
+from aiogram.methods.delete_message import DeleteMessage
 from aiogram.filters import Command
 from aiogram.filters import MagicData
 
-from config import cfg
+from config import cfg, main_keyboard, play_keyboard, cards_check
 
 # Bot token can be obtained via https://t.me/BotFather
 TOKEN = cfg["TOKEN"]
 
 # All handlers should be attached to the Router (or Dispatcher)
 
-maintenance_router = Router()
-maintenance_router.message.filter(MagicData(F.maintenance_mode.is_(True)))
-maintenance_router.callback_query.filter(MagicData(F.maintenance_mode.is_(True)))
-
-router = Router()
-
 dp = Dispatcher()
-#dp.include_routers(router)
 
-allCards_bt = KeyboardButton(text='ВСЕ МОИ КАРТЫ')
-startGame_bt = KeyboardButton(text='Начать игру')
-stats_bt = KeyboardButton(text='Моя статистика')
-main_kb_buttons = [
-    [allCards_bt],
-    [startGame_bt],
-    [stats_bt]
-]
-main_kb: ReplyKeyboardMarkup = ReplyKeyboardMarkup(keyboard=main_kb_buttons)
+
 
 
 @dp.message(CommandStart())
@@ -52,13 +37,20 @@ async def command_start_handler(message: Message) -> None:
     await message.answer(f"Привет, {html.bold(message.from_user.full_name)}! Этот игровой бот сыграет с тобой с "
                          f"усложненную версию игры - камень, ножницы, бумага. Так же вам будет представлен полностью "
                          f"новые ключевые предметы для игры. И со временем Вам откроются новые, надо лишь играть!)",
-                         reply_markup=main_kb)
+                         reply_markup=main_keyboard())
 
 
-@dp.message(F.text == "ВСЕ МОИ КАРТЫ")
-async def callback_query_handler(callback: CallbackQuery):
-    await callback.answer("Ваши карты!)")
-    await callback.message.answer("CARDS")
+@dp.message(F.text == "Все мои карты")
+async def send_all_your_cards(mes: Message, bot: Bot):
+    await mes.answer("Ваши карты:")
+    photo1 = InputMediaPhoto(type='photo', media=FSInputFile(r'icons/rock.jpg'))
+    photos = [photo1]
+    await bot.send_media_group(mes.chat.id, photos) #сделать систему этих карточек
+
+@dp.message(F.text == "Начать игру")
+async def send_all_your_cards(mes: Message, bot: Bot):
+    await mes.answer("Игра началась...")
+    await mes.answer("Выберите свою карту", reply_markup=play_keyboard())
 
 
 async def main() -> None:
