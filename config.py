97fa372b7d8eef88bca234ebcea197f47bfd4697
miusGG <romanovegorsergeevich@gmail.com>
2024-12-10 09:02:39 +0300

    1.0

--- config.py
+++ config.py
@@ -1,22 +1,33 @@
+#  общий конфиг куда можно добавить какую либо информацию о бота, сейчас там токен
 cfg = {
     "TOKEN": "7921775209:AAHSjWnM5_9cigxHu9TrRJqIXMQnibuwfos"
 }
-my_cards = {
-    "rock": "icons/rock.jpg', 'rb",
-    "paper": '',
-    "scissors": '',
-}
 
+#  словарь для понимания какие карты есть у пользователя
 cards_check = {
     "rock": True,
     "paper": True,
-    "scissors": True
+    "scissors": True,
+    "axe": True,
+    "picaxe": True
 }
 
+#  изображение для каждой карты
+images_cards = {
+    "rock": "icons/rock.jpg",
+    "paper": "icons/paper.PNG",
+    "scissors": "icons/scissors.PNG",
+    "axe": "icons/axe.PNG",
+    "picaxe": "icons/picaxe.PNG"
+}
+
+# далее идут доп функции для бота и клавиатуры
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 from aiogram import types
 
+
+# клавиатруы:
 def main_keyboard():
     allCards_bt = KeyboardButton(text='Все мои карты')
     startGame_bt = KeyboardButton(text='Начать игру')
@@ -31,11 +42,12 @@ def main_keyboard():
                                                        input_field_placeholder="Камень, ножницы, бумага раз....два....ТРИ")
     return main_kb
 
+
 def play_keyboard():
     builder = InlineKeyboardBuilder()
     builder.add(types.InlineKeyboardButton(
         text="Rock",
-        callback_data="rock")
+        callback_data="rock"),
     )
     builder.add(types.InlineKeyboardButton(
         text="Scissors",
@@ -45,4 +57,62 @@ def play_keyboard():
         text="Paper",
         callback_data="paper")
     )
+    builder.add(types.InlineKeyboardButton(
+        text="Axe",
+        callback_data="axe")
+    )
+    builder.add(types.InlineKeyboardButton(
+        text="Picaxe",
+        callback_data="picaxe")
+    )
     return builder.as_markup()
+
+
+def duel_keybord():
+    builder = InlineKeyboardBuilder()
+    builder.add(types.InlineKeyboardButton(
+        text="СЫГРАТЬ ЭТОЙ КАРТОЙ!",
+        callback_data="duel"),
+    )
+    return builder.as_markup()
+
+
+# функции для обновления побед порожений и игр
+def update_game_stats(filepath="stats.txt"):
+    """
+    Читает статистику игр из файла, обновляет ее после победы и записывает обратно.
+    """
+    try:
+        with open(filepath, "r") as f:
+            games, wins, losses = map(int, f.readline().split())
+    except FileNotFoundError:
+        games, wins, losses = 0, 0, 0
+        print("Файл со статистикой не найден. Создан новый файл.")
+
+    games += 1
+    wins += 1
+
+    with open(filepath, "w") as f:
+        f.write(f"{games} {wins} {losses}")
+
+    print(f"Статистика обновлена: Игры - {games}, Победы - {wins}, Поражения - {losses}")
+
+
+def update_game_stats_loss(filepath="stats.txt"):
+    """
+    Читает статистику игр из файла, обновляет ее после поражения и записывает обратно.
+    """
+    try:
+        with open(filepath, "r") as f:
+            games, wins, losses = map(int, f.readline().split())
+    except FileNotFoundError:
+        games, wins, losses = 0, 0, 0
+        print("Файл со статистикой не найден. Создан новый файл.")
+
+    games += 1
+    losses += 1
+
+    with open(filepath, "w") as f:
+        f.write(f"{games} {wins} {losses}")
+
+    print(f"Статистика обновлена: Игры - {games}, Победы - {wins}, Поражения - {losses}")
--- main.py
+++ main.py
@@ -1,38 +1,29 @@
 import asyncio
 import logging
 import sys
-from os import getenv
-
 from aiogram import Bot, Dispatcher, html
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart
 from aiogram.types import Message
-
-from aiogram.types import ReplyKeyboardRemove, \
-    ReplyKeyboardMarkup, KeyboardButton, \
-    InlineKeyboardMarkup, InlineKeyboardButton
-
 from aiogram.types import CallbackQuery, InputMediaPhoto, FSInputFile
-
-from aiogram import Router, F
-from aiogram.methods.delete_message import DeleteMessage
-from aiogram.filters import Command
-from aiogram.filters import MagicData
-
-from config import cfg, main_keyboard, play_keyboard, cards_check
+from aiogram import F
+from config import cfg, main_keyboard, play_keyboard, cards_check, update_game_stats_loss, update_game_stats, \
+    duel_keybord, images_cards
+from checkHowWin import check
+import random
 
 # Bot token can be obtained via https://t.me/BotFather
-TOKEN = cfg["TOKEN"]
+TOKEN = cfg["TOKEN"]  # токен для работы бота в телеграмм
 
 # All handlers should be attached to the Router (or Dispatcher)
 
 dp = Dispatcher()
+all_cards = list(images_cards.keys())  # все карты
+temp = "rock"  # временная переменная для хранение нашей карты
 
 
-
-
-@dp.message(CommandStart())
+@dp.message(CommandStart())  # Команда для /start для бота
 async def command_start_handler(message: Message) -> None:
     await message.answer(f"Привет, {html.bold(message.from_user.full_name)}! Этот игровой бот сыграет с тобой с "
                          f"усложненную версию игры - камень, ножницы, бумага. Так же вам будет представлен полностью "
@@ -41,23 +32,122 @@ async def command_start_handler(message: Message) -> None:
 
 
 @dp.message(F.text == "Все мои карты")
-async def send_all_your_cards(mes: Message, bot: Bot):
+async def send_all_your_cards(mes: Message, bot: Bot):  # выврд всех имющийхся карт у пользователя
     await mes.answer("Ваши карты:")
-    photo1 = InputMediaPhoto(type='photo', media=FSInputFile(r'icons/rock.jpg'))
-    photos = [photo1]
-    await bot.send_media_group(mes.chat.id, photos) #сделать систему этих карточек
+    photos = []
+    if cards_check["rock"]:
+        photoRock = InputMediaPhoto(type='photo', media=FSInputFile(r'icons/rock.jpg'))
+        photos.append(photoRock)
+    if cards_check["paper"]:
+        photoPaper = InputMediaPhoto(type='photo', media=FSInputFile(r'icons/paper.PNG'))
+        photos.append(photoPaper)
+    if cards_check["scissors"]:
+        photoScissors = InputMediaPhoto(type='photo', media=FSInputFile(r'icons/scissors.PNG'))
+        photos.append(photoScissors)
+    if cards_check["axe"]:
+        photoAxe = InputMediaPhoto(type='photo', media=FSInputFile(r'icons/axe.PNG'))
+        photos.append(photoAxe)
+    if cards_check["picaxe"]:
+        photoPicaxe = InputMediaPhoto(type='photo', media=FSInputFile(r'icons/picaxe.PNG'))
+        photos.append(photoPicaxe)
+    await bot.send_media_group(mes.chat.id, photos)  # сделать систему этих карточек и отправить
+
 
 @dp.message(F.text == "Начать игру")
-async def send_all_your_cards(mes: Message, bot: Bot):
+async def send_all_your_cards(mes: Message, bot: Bot):  # команда для начала дуэли
     await mes.answer("Игра началась...")
-    await mes.answer("Выберите свою карту", reply_markup=play_keyboard())
+    await mes.answer("Выберите свою карту:", reply_markup=play_keyboard())
 
 
-async def main() -> None:
+@dp.message(F.text == 'Моя статистика')
+async def send_all_your_cards(mes: Message, bot: Bot):  # команда для общей статистики 1 игрока
+    with open("stats.txt", "r") as f:
+        games, wins, losses = map(int, f.readline().split())
+    await mes.answer(f"Победы: {wins}\nПоражения:{losses}\nВсего игр:{games}")
+
+
+# далее идут команды для выбора карты во время дуэли
+@dp.callback_query(F.data == "rock")
+async def rock_callback(callback: CallbackQuery, bot: Bot):
+    await callback.answer("Вы выбрали КАМЕНЬ!")
+    await callback.message.answer("КАМЕНЬ", reply_markup=duel_keybord())  # добавление клавиатуры
+    photoRock = InputMediaPhoto(type='photo', media=FSInputFile(r'icons/rock.jpg'))  # вывод карты картинки
+    photos = [photoRock]
+    await bot.send_media_group(callback.message.chat.id, photos)
+    global temp
+    temp = "rock"  # запись карты в переменную         Далее все аналогично:
+
+
+@dp.callback_query(F.data == "paper")
+async def rock_callback(callback: CallbackQuery, bot: Bot):
+    await callback.answer("Вы выбрали БУМАГУ!")
+    await callback.message.answer("БУМАГА", reply_markup=duel_keybord())
+    photo = InputMediaPhoto(type='photo', media=FSInputFile(r'icons/paper.PNG'))
+    photos = [photo]
+    await bot.send_media_group(callback.message.chat.id, photos)
+    global temp
+    temp = "paper"
+
+
+@dp.callback_query(F.data == "scissors")
+async def rock_callback(callback: CallbackQuery, bot: Bot):
+    await callback.answer("Вы выбрали НОЖНИЦЫ!")
+    await callback.message.answer("НОЖНИЦЫ", reply_markup=duel_keybord())
+    photo = InputMediaPhoto(type='photo', media=FSInputFile(r'icons/scissors.PNG'))
+    photos = [photo]
+    await bot.send_media_group(callback.message.chat.id, photos)
+    global temp
+    temp = "scissors"
+
+
+@dp.callback_query(F.data == "axe")
+async def rock_callback(callback: CallbackQuery, bot: Bot):
+    await callback.answer("Вы выбрали ТОПОР!")
+    await callback.message.answer("ТОПОР", reply_markup=duel_keybord())
+    photo = InputMediaPhoto(type='photo', media=FSInputFile(r'icons/axe.PNG'))
+    photos = [photo]
+    await bot.send_media_group(callback.message.chat.id, photos)
+    global temp
+    temp = "axe"
+
+
+@dp.callback_query(F.data == "picaxe")
+async def rock_callback(callback: CallbackQuery, bot: Bot):
+    await callback.answer("Вы выбрали КИРКУ!")
+    await callback.message.answer("КИРКА", reply_markup=duel_keybord())
+    photo = InputMediaPhoto(type='photo', media=FSInputFile(r'icons/picaxe.PNG'))
+    photos = [photo]
+    await bot.send_media_group(callback.message.chat.id, photos)
+    global temp
+    temp = "picaxe"
+
+
+@dp.callback_query(F.data == "duel")
+async def duel(callback: CallbackQuery, bot: Bot):
+    temp_cards = all_cards
+    temp_cards.remove(temp)  # создаю массив без карты которую выбрал игрок
+    print("Ряд карт на рассмотрение для врага", temp_cards)
+    en_card = random.choice(temp_cards)  # выбор карты для врага
+    await callback.message.answer("Карта врага!")
+    photo = InputMediaPhoto(type='photo', media=FSInputFile(rf"{images_cards[en_card]}"))
+    photos = [photo]
+    await bot.send_media_group(callback.message.chat.id, photos) # вывод карты врага
+    if check(temp, en_card):  # отпределение победителя или проигравшего:
+        await callback.message.answer("ВЫ ПОБЕДИЛИ!!!")
+        update_game_stats()  # обновление статистики
+    elif not check(temp, en_card):
+        await callback.message.answer("ВЫ ПРОИГРАЛИ.")
+        update_game_stats_loss()   # обновление статистики
+    else:  # если что то пойдет не так будет выведена ошибка
+        print("Неизвестная ошибка")
+        await callback.message.answer("Неизвестная ошибка... Попробуйте еще раз")
+
+
+async def main() -> None:  # функция для запуска бота
     bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
     await dp.start_polling(bot, dispatcher=dp)
 
 
-if __name__ == "__main__":
+if __name__ == "__main__":  # его запуск
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
     asyncio.run(main())
